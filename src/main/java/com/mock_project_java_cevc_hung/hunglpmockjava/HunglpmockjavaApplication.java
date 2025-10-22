7de2ba22324ee8efb95599cd145e3c31c98994eb
hunglp-3071 <le.phu.hung@sun-asterisk.com>
2025-10-22 14:53:45 +0700

    init project

---
+++ src/main/java/com/mock_project_java_cevc_hung/hunglpmockjava/HunglpmockjavaApplication.java
@@ -0,0 +1,13 @@
+package com.mock_project_java_cevc_hung.hunglpmockjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunglpmockjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HunglpmockjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mock_project_java_cevc_hung/hunglpmockjava/HunglpmockjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mock_project_java_cevc_hung.hunglpmockjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HunglpmockjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
