3470a6e7a3a8f9d52584b7b87be6dcc48b19f2b2
hunglp-3071 <le.phu.hung@sun-asterisk.com>
2025-10-22 14:47:10 +0700

    init project

---
+++ springbootmock/src/test/java/com/mockspringboot/springbootmock/SpringbootmockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockspringboot.springbootmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
